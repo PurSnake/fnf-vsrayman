78797ebdaa757e92e2c2590960dbac263eb24172
PurSnake <56314743+PurpleSSSnake@users.noreply.github.com>
2022-05-07 18:36:17 +0300

    Update EngineData.hx

--- source/data/EngineData.hx
+++ source/data/EngineData.hx
@@ -2,7 +2,7 @@ package data;
 
 class EngineData
 {
-    public static var grafexEngineVersion:String = '1.0.0';
+    public static var grafexEngineVersion:String = '0.2.3';
     public static var raymanmodver:String = '1b'; //This is also used for Discord RPC  || Maybe, why not ¯\_(ツ)_/¯ - Snake
 }
 
